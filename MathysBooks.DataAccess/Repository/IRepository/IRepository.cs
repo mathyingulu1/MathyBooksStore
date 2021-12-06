6d8da320e301b0c0d6f41e80a0e604028293ec95
Mathy Ingulu <W0776344@myscc.ca>
2021-12-05 22:03:14 -0500

    adding  ICategoryRepository Category { get; }         ICoverTypeRepository CoverType { get; }         IProductRepository Product { get; }

--- MathysBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ MathysBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
-//using System.Threading.Tasks;
+using System.Threading.Tasks;
 
 namespace MathysBooks.DataAccess.Repository.IRepository
 {
--- MathysBooks.DataAccess/Repository/IRepository/IUnitofWork.cs
+++ MathysBooks.DataAccess/Repository/IRepository/IUnitofWork.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace MathysBooks.DataAccess.Repository.IRepository
 {
-    interface IUnitofWork : IDisposable
+    public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
-
+        ICoverTypeRepository CoverType { get; }
+        IProductRepository Product { get; }
         ISP_Call SP_Call { get; }
+
+        void Save();
     }
 }
